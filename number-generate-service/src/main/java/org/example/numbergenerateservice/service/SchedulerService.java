28463a58dadbd08091a646d14e693990570ac602
D3L1K4T3S <zhelagin.egor@yandex.ru>
2024-07-20 14:47:17 +0300

    Modify a project

--- number-generate-service/src/main/java/org/example/numbergenerateservice/service/SchedulerService.java
+++
@@ -1,7 +0,0 @@
-package org.example.numbergenerateservice.service;
-
-import java.util.concurrent.TimeUnit;
-
-public interface SchedulerService {
-    void addTaskToDeleteAfterTime(Runnable task, long delay, TimeUnit timeUnit);
-}
